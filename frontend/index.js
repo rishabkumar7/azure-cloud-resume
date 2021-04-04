0b08e582452d68a18177a6e676081dc182198a8b
Rishab Kumar <rishabkumar7@gmail.com>
2021-04-03 21:31:21 -0400

    resume website - frontend

---
+++ frontend/index.js
@@ -0,0 +1,55 @@
+$(document).ready(function (e) {
+    $win = $(window);
+    $navbar = $('#header');
+    $toggle = $('.toggle-button');
+    var width = $navbar.width();
+    toggle_onclick($win, $navbar, width);
+
+    // resize event
+    $win.resize(function () {
+        toggle_onclick($win, $navbar, width);
+    });
+
+    $toggle.click(function (e) {
+        $navbar.toggleClass("toggle-left");
+    })
+
+});
+
+function toggle_onclick($win, $navbar, width) {
+    if ($win.width() <= 768) {
+        $navbar.css({ left: `-${width}px` });
+    } else {
+        $navbar.css({ left: '0px' });
+    }
+}
+
+var typed = new Typed('#typed', {
+    strings: [
+        'Cloud Engineer',
+        'DevOps Engineer'
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
+
+var typed_2 = new Typed('#typed_2', {
+    strings: [
+        'Cloud Engineer',
+        'DevOps Engineer'
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
