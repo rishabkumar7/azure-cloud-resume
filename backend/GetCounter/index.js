0edeaa073619800299dad2c1828a85c5646861b4
Rishab Kumar <rishabkumar7@gmail.com>
2021-05-11 13:51:49 -0400

    Updated the function

--- backend/GetCounter/index.js
+++ backend/GetCounter/index.js
@@ -1,21 +1,7 @@
-module.exports = async function (context) {
-    context.log("JavaScript HTTP trigger function processed a request.");
-
-    context.log(context.bindings.inputDocument);
-    // For first time creation of record
-    context.bindings.outputDocument = context.bindings.inputDocument || { id: "1", count: 0 };
-
-    //Increment by 1
-    context.bindings.outputDocument.count = context.bindings.outputDocument.count + 1;
-    context.log(context.bindings.outputDocument);
+module.exports = async function (context, req, data) {
+    context.bindings.outputDocument = data[0];
+    context.bindings.outputDocument.count += 1;
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        headers: {
-            "content-type": "application/json"
-        },
-        body: {
-            id: context.bindings.outputDocument.id,
-            count: context.bindings.outputDocument.count
-        }
+        body: context.bindings.outputDocument.count
     };
 }
